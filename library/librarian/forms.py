768d938329fa53b1b906fdfdc28db3f4b8700a08
AnneVi <villette55@gmail.com>
2024-10-22 21:15:07 +0200

    add gestion des emprunts

--- library/librarian/forms.py
+++ library/librarian/forms.py
@@ -19,3 +19,44 @@ class MemberForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+
+from django import forms
+from shared_models.models import Member, Book, DVD, CD
+
+class LoanForm(forms.Form):
+    member = forms.ModelChoiceField(
+        queryset=Member.objects.all(),
+        label="Membre"
+    )
+    item_type = forms.ChoiceField(
+        choices=[
+            ('', '---'),
+            ('book', 'Livre'),
+            ('dvd', 'DVD'),
+            ('cd', 'CD')
+        ],
+        label="Type de média"
+    )
+    item = forms.ModelChoiceField(
+        queryset=None,  # On va le définir dynamiquement
+        label="Item",
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Si le formulaire est soumis avec un type de média
+        if 'item_type' in self.data:
+            try:
+                item_type = self.data.get('item_type')
+                if item_type == 'book':
+                    self.fields['item'].queryset = Book.objects.filter(is_available=True)
+                elif item_type == 'dvd':
+                    self.fields['item'].queryset = DVD.objects.filter(is_available=True)
+                elif item_type == 'cd':
+                    self.fields['item'].queryset = CD.objects.filter(is_available=True)
+            except (ValueError, TypeError):
+                pass
+        else:
+            self.fields['item'].queryset = Book.objects.none()  # Champ vide par défaut
--- library/librarian/urls.py
+++ library/librarian/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('member_list/', views.member_list, name='member_list'),
     path('add_member/', views.add_member, name='add_member'),
     path('member/<int:member_id>/', views.member_detail, name='member_detail'),
+    path('borrow/', views.borrow_item, name='borrow_item'),
+    path('return/', views.return_list, name='return_item'),  # Vue de la liste des retours
+    path('return/<int:loan_id>/confirm/', views.confirm_return, name='confirm_return'),  # Pour confirmer un retour spécifique
 ]
